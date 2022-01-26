e5f460a36e6c551c78e1791358ca1f5ec248ac93
Alexey Rudoy <rudenw01@gmail.com>
2022-01-26 18:36:55 +0300

    fix: web.xml, pom.xml

--- src/main/java/by/academy/controller/Controller.java
+++ src/main/java/by/academy/controller/Controller.java
@@ -5,8 +5,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Controller extends HttpServlet {
     public static final long serialVersionUID = 1L;
